fe76b38b59c3bdc02afea601ece036346f602bd9
Peredi06 <163517062+Peredi06@users.noreply.github.com>
2025-01-22 21:29:17 +0000

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,31 @@
+var MenuItens = document.getElementById("MenuItens");
+
+MenuItens.style.maxHeight = "0px";
+
+function menucelular(){
+    if(MenuItens.style.maxHeight == "0px"){
+        MenuItens.style.maxHeight = "200px";
+    }else{
+        MenuItens.style.maxHeight = "0px";
+    }
+}
+
+var EntrarPainel = document.getElementById("EntrarPainel");
+var Registo = document.getElementById("Registo");
+var Indicador = document.getElementById("Indicador");
+
+function Registar()
+{
+    Registo.style.transform = "translateX(0px)";
+    EntrarPainel.style.transform = "translateX(0px)";
+    Indicador.style.transform = "translateX(100px)";
+}
+
+function Entrar()
+{
+    Registo.style.transform = "translateX(300px)";
+    EntrarPainel.style.transform = "translateX(300px)";
+    Indicador.style.transform = "translateX(0px)";
+}
+
+
